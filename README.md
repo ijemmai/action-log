0c90b401730d4ae9860dd1c4deb337a97417f6b0
ijemmai <duckcity8000@gmail.com>
2025-11-24 12:37:52 +0200

    posts and comments are now cached

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-A simple app that logs mod actions to a discord channel using webhooks
-currently supported actions:
+A simple app that logs mod actions to a discord channel using webhooks  
+currently supported actions:  
 
 - member bans
 - member unbans
 - post removals
 - comment removals
+- mod notes 
+- removal reasons
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { Devvit, EventTypes, type FormField } from "@devvit/public-api";
+import { Devvit } from "@devvit/public-api";
 
 Devvit.configure({
   redditAPI: true,
+  redis: true,
   http: true
 });
 
@@ -75,6 +76,7 @@ Devvit.addTrigger({
     const logRemovalReasons = await context.settings.get("log-removal-reason")
     const webhoookLink: string = (await context.settings.get("discord-webhook"))!
     let payload = null
+
     if (event.action === "addremovalreason" && logRemovalReasons) {
       const comments = context.reddit.getModerationLog({ subredditName: event.subreddit!.name, type: "addremovalreason", "moderatorUsernames": [event.moderator!.name] })
       const comment = (await comments.get(1))[0]
@@ -96,6 +98,7 @@ Devvit.addTrigger({
         ]
       }
     }
+
     if (event.action === "addnote" && logModNotes) {
       const notes = context.reddit.getModerationLog({ subredditName: event.subreddit!.name, type: "addnote", "moderatorUsernames": [event.moderator!.name] })
       const note = (await notes.get(1))[0]
@@ -116,7 +119,20 @@ Devvit.addTrigger({
         ]
       }
     }
+
     if (event.action === "removelink" && logPostRemovals) {
+      let post;
+      try {
+        post = JSON.parse((await context.redis.get(event.targetPost!.id))!)
+      } catch (error) {
+        post = {
+          author: event.targetUser?.name,
+          title: event.targetPost?.title,
+          body: event.targetPost?.selftext || "Post Body is either empty, or the post only contains attachments",
+          permalink: event.targetPost?.permalink,
+          subreddit: event.subreddit?.name
+        }
+      }
       payload = {
         avatar_url: subredditIcon,
         username: subreddit.name,
@@ -125,19 +141,31 @@ Devvit.addTrigger({
             color: 16729344,
             title: "📋 Post Deleted",
             fields: [
-              { name: "Title", value: event.targetPost?.title },
-              { name: "Author", value: `u/${event.targetUser?.name}` },
-              { name: "Body", value: event.targetPost?.selftext || "Empty Post" },
+              { name: "Title", value: post.title },
+              { name: "Author", value: `u/${post.author}` },
+              { name: "Body", value: post.body },
               { name: "Responsible Moderator", value: `u/${event.moderator?.name}` },
-              { name: "Permalink", value: `[link](https://reddit.com${event.targetPost?.permalink})` },
-              { name: "Subreddit", value: `r/${event.subreddit?.name}` },
+              { name: "Permalink", value: `[link](https://reddit.com${post.permalink})` },
+              { name: "Subreddit", value: `r/${post.subreddit}` },
             ],
             footer: { text: `postID: ${event.targetPost?.id}` }
           }
         ]
       }
     }
+
     if (event.action === "removecomment" && logCommenRemovals) {
+      let comment;
+      try {
+        comment = JSON.parse((await context.redis.get(event.targetComment!.id))!)
+      } catch (error) {
+        comment = {
+          author: event.targetUser?.name,
+          body: event.targetComment?.body,
+          permalink: event.targetComment?.permalink,
+          subreddit: event.targetComment?.subredditId
+        }
+      }
       payload = {
         avatar_url: subredditIcon,
         username: subreddit.name,
@@ -146,22 +174,24 @@ Devvit.addTrigger({
             color: 16729344,
             title: "💬 Comment Deleted",
             fields: [
-              { name: "Author", value: `u/${event.targetUser?.name}` },
-              { name: "Conent", value: event.targetComment?.body },
+              { name: "Author", value: `u/${comment.author}` },
+              { name: "Conent", value: comment.body },
               { name: "Responsible Moderator", value: `u/${event.moderator?.name}` },
-              { name: "Permalink", value: `[link](https://reddit.com${event.targetComment?.permalink})` },
-              { name: "Subreddit", value: `r/${event.subreddit?.name}` },
+              { name: "Permalink", value: `[link](https://reddit.com${comment.permalink})` },
+              { name: "Subreddit", value: `r/${comment.subreddit}` },
             ],
             footer: { text: `commentID: ${event.targetComment?.id}` }
           }
         ]
       }
     }
+
     if (event.action === "banuser" && logBans) {
       let users;
       let user;
       try {
-        users = context.reddit.getModerationLog({ subredditName: event.subreddit!.name, type: "banuser", "moderatorUsernames": [event.moderator!.name] })
+        const subreddit = await context.reddit.getCurrentSubreddit()
+        users = subreddit.getModerationLog({ type: "banuser", "moderatorUsernames": [event.moderator!.name] })
         user = (await users.get(1))[0]
       }
       catch {
@@ -185,6 +215,7 @@ Devvit.addTrigger({
         ]
       }
     }
+
     if (event.action === "unbanuser" && logUnBans) {
       payload = {
         avatar_url: subredditIcon,
@@ -201,7 +232,6 @@ Devvit.addTrigger({
           }
         ]
       }
-
     }
     if (payload && webhoookLink) {
       try {
@@ -217,6 +247,35 @@ Devvit.addTrigger({
         console.log(e)
       }
     }
+  }
+})
+
+
+Devvit.addTrigger({
+  events: ["PostCreate", "PostSubmit"],
+  onEvent: async (event, context) => {
+    console.log("caching comment")
+    await context.redis.set(event.post!.id, JSON.stringify({
+      author: event.author?.name,
+      title: event.post?.title,
+      body: event.post?.selftext || "Post Body is either empty, or the post only contains attachments",
+      permalink: event.post?.permalink,
+      subreddit: event.subreddit?.name
+    }))
+    await context.redis.expire(event.post!.id, 60 * 60 * 24 * 30)
+  }
+})
+
+Devvit.addTrigger({
+  events: ["CommentCreate", "CommentSubmit"],
+  onEvent: async (event, context) => {
+    await context.redis.set(event.comment!.id, JSON.stringify({
+      author: event.author?.name,
+      body: event.comment?.body || "Comment either empty or only contains media",
+      permalink: event.comment?.permalink,
+      subreddit: event.subreddit?.name
+    }))
+    await context.redis.expire(event.post!.id, 60 * 60 * 24 * 30)
   }
 })
 
